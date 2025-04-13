7432a0e35b1228f9368433af0ebdb6b61737e290
Akshit1311 <a4748g@gmail.com>
2025-04-13 14:43:22 +0530

    update: receive proof api

--- client/src/app/api/receive-proofs/route.ts
+++ client/src/app/api/receive-proofs/route.ts
@@ -7,17 +7,8 @@ export async function GET() {
 }
 export async function POST(req: Request) {
 	const data = await req.json();
-	const decodedBody = decodeURIComponent(data);
-	const proof = JSON.parse(decodedBody);
-	const result = await verifyProof(proof);
 
-	if (!result) {
-		return new Response("Invalid proofs data", {
-			status: 400,
-		});
-	}
+	console.log({ data });
 
-	console.dir(proof, { depth: null });
-
-	return Response.json({ proof });
+	return Response.json({ data });
 }
