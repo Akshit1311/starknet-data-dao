2be7fe9889e3f97448a0a6725ed43a7482b838a3
Hemant <hrsmart2150@gmail.com>
2025-04-15 22:03:57 +0530

    chore: added users in leaderboard table

--- client/src/app/leaderboard/_components/table.tsx
+++ client/src/app/leaderboard/_components/table.tsx
@@ -26,45 +26,11 @@ import {
 
 import { columns, type LeaderboardData } from "./columns";
 
-const data: LeaderboardData[] = [
-	{
-		id: "1",
-		rank: 1,
-		name: "John Doe",
-		address: "0x1234567890abcdef1234567890abcdef12345678",
-		points: "1000",
-	},
-	{
-		id: "2",
-		rank: 2,
-		name: "Jane Smith",
-		address: "0xabcdef1234567890abcdef1234567890abcdef12",
-		points: "900",
-	},
-	{
-		id: "3",
-		rank: 3,
-		name: "Alice Johnson",
-		address: "0x7890abcdef1234567890abcdef1234567890abcd",
-		points: "800",
-	},
-	{
-		id: "4",
-		rank: 4,
-		name: "Bob Brown",
-		address: "0x4567890abcdef1234567890abcdef1234567890",
-		points: "700",
-	},
-	{
-		id: "5",
-		rank: 5,
-		name: "Charlie Davis",
-		address: "0x234567890abcdef1234567890abcdef12345678",
-		points: "600",
-	},
-];
+interface LeaderboardTableProps {
+	data: LeaderboardData[];
+}
 
-const LeaderboardTable = () => {
+const LeaderboardTable: React.FC<LeaderboardTableProps> = ({ data }) => {
 	const [sorting, setSorting] = React.useState<SortingState>([]);
 	const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
 		[],
--- client/src/app/leaderboard/page.tsx
+++ client/src/app/leaderboard/page.tsx
@@ -2,9 +2,23 @@ import type { NextPage } from "next";
 import React from "react";
 import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
+import { shortAddress } from "~/lib/utils";
+import { api } from "~/trpc/server";
+
+import type { LeaderboardData } from "./_components/columns";
 import LeaderboardTable from "./_components/table";
 
-const LeaderboardPage: NextPage = () => {
+const LeaderboardPage: NextPage = async () => {
+	const users = await api.auth.users();
+
+	const formattedData: LeaderboardData[] = users.map((user, idx) => ({
+		id: user.id.toString(),
+		rank: idx + 1,
+		name: user.nickname || `Anonymous${idx + 1}`,
+		address: shortAddress(user.address),
+		points: "1000",
+	}));
+
 	return (
 		<main className="flex flex-col items-center justify-center bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px] min-h-[100dvh] bg-background">
 			<div className="mx-auto max-w-5xl w-full flex flex-col items-center gap-12">
@@ -20,7 +34,7 @@ const LeaderboardPage: NextPage = () => {
 						</RoughNotation>
 					</h1>
 				</RoughNotationGroup>
-				<LeaderboardTable />
+				<LeaderboardTable data={formattedData} />
 			</div>
 		</main>
 	);
