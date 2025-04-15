aceb16efe81927cdfdb36753dd6e8a149b8d202f
Hemant <hrsmart2150@gmail.com>
2025-04-15 16:53:52 +0530

    chore: font updates

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<html lang="en" className={`${geist.variable}`}>
-			<body>
+			<body className="font-jetbrainsMono">
 				<TRPCReactProvider>
 					<Providers>
 						<Navbar />
